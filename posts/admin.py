186ab17f58f7b8c0cf43d7ca820285fa25310275
Adam <spinning210@gmail.com>
2020-11-13 14:19:50 +0800

    admin後台創建

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Location, Post
 # Register your models here.
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'content', 'author', 'location')
+    exclude = ('create_date', )
+    #fields = ('subject', 'content', 'author', 'location')
+
+admin.site.register(Location, LocationAdmin)  #註冊至Administration(管理員後台)
+admin.site.register(Post, PostAdmin)  #註冊至Administration(管理員後台)
